c125d2efb5ef7f223888b1429d9252785e9908e7
thecubenoob <148161878+thecubenoob@users.noreply.github.com>
2023-11-26 14:45:50 +1100

    Update keep_alive.go

--- packet/keep_alive.go
+++ packet/keep_alive.go
@@ -1,19 +1,19 @@
 package packet
 
 type KeepAliveServer struct {
-	PayloadID int32
+	PayloadID int64
 }
 
-func (a *KeepAliveServer) ID() int32 {
-	return 0x12
+func (a *KeepAliveServer) ID() int64 {
+	return 0x21
 }
 
 func (a *KeepAliveServer) Encode(w Writer) error {
-	return w.Int32(a.PayloadID)
+	return w.Int64(a.PayloadID)
 }
 
 func (a *KeepAliveServer) Decode(r *Reader) error {
-	return r.Int32(&a.PayloadID)
+	return r.Int64(&a.PayloadID)
 }
 
 type KeepAliveClient struct {
